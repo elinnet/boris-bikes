3294024f5e800af7dbe923a25e43fd9f79aa6e83
Elin <elinnet@gmail.com>
2015-06-11 11:37:45 +0100

    boris bikes - add no bikes available feature

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,8 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'release working bike' do
-  	bike = subject.release_bike
+  	subject.dock Bike.new
+    bike = subject.release_bike
   	expect(bike).to be_working
   end
 
