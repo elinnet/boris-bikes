d380485c08f9216d8161fc2fb909ceaf5f0ae7f4
Triffanys101 <l.hargrave@hotmail.co.uk>
2015-06-11 12:31:23 +0100

    raised error message to 20 bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,6 +17,13 @@ describe DockingStation do
       expect {subject.release_bike}.to raise_error 'No bikes available'
     end    
   end
+
+  describe 'dock' do
+    it 'raises an error when full' do
+      20.times { subject.dock Bike.new }
+      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+    end
+  end
 end
 
  
